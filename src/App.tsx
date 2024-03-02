790b9d21b9e05150118799ca881f52ad6a899e98
vielfras <vielfras@gmail.com>
2024-03-02 19:59:40 +0200

    fixed compilation error

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,10 @@ import './App.css'
 
 import Default from './layouts/Default/Default'
 
-import UserContext from './store/user-contect.js'
-
 export default function App() {
-
-  const 
   return (
     <>
-      <UserContext.Provider value={{
-        user: {email: '', password: ''},
-        isLoggedIn: false,
-        login: null,
-        
-
-      }}>
-        <Default></Default>
-      </UserContext.Provider>
+      <Default></Default>
     </>
   )
 }
