14ebd1706f11e9ad0656814df2b2195063851636
vielfras <vielfras@gmail.com>
2024-03-24 19:37:05 +0200

    added doUpdateCard

--- src/services/CardsService.ts
+++ src/services/CardsService.ts
@@ -2,6 +2,26 @@ import { apiBase } from "../config";
 import { ICard } from "../interfaces/CardInterfaces";
 import { getToken } from "./UserService";
 
+
+export const doGetCardById = async (cardId: string): Promise<{ error: string | null, result: ICard | null }> => {
+  try {
+    const response = await fetch(`${apiBase}/cards/${cardId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    if (!response.ok) {
+      return { error: data.message || 'Failed to fetch card', result: null };
+    }
+    return { error: null, result: data };
+  } catch (err) {
+    const errMessage = (err as Error).message;
+    return { error: errMessage, result: null };
+  }
+};
+
 // ---------------------------------------------------------------------------------------------------------
 
 export const doGetAllCards = async (): Promise<{ error: string | null, result: ICard[] | null }> => {
@@ -101,6 +121,37 @@ export const doDeleteCard = async (cardId: string, bizNumber: number): Promise<{
     return { error: null, result: data };
   }
   catch (err) {
+    const errMessage = (err as Error).message;
+    return { error: errMessage, result: null };
+  }
+};
+
+// ---------------------------------------------------------------------------------------------------------
+
+export const doUpdateCard = async (cardId: string, cardData: ICard): Promise<{ error: string | null; result: ICard | null }> => {
+  try {
+    const token = await getToken();
+    if (!token) {
+      return { error: 'Authentication required. No token found.', result: null };
+    }
+
+    const response = await fetch(`${apiBase}/cards/${cardId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-auth-token': token,
+      },
+      body: JSON.stringify(cardData),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      return { error: data.message || 'Failed to update card', result: null };
+    }
+
+    return { error: null, result: data };
+  } catch (err) {
     const errMessage = (err as Error).message;
     return { error: errMessage, result: null };
   }
